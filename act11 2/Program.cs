07edd9bdbb088c2320133c806599e0978e627b1c
CDVCvask <cesarcamposeco415@gmail.com>
2026-03-05 20:32:00 -0600

    primero

---
+++ act11 2/Program.cs
@@ -0,0 +1,12 @@
+﻿void mostrarnombre(string nombre)
+{
+    Console.WriteLine("Hola, "+nombre );
+}
+
+static void main()
+{
+    Console.WriteLine("Ingrese su nombre:");
+    string nombre1 = Console.ReadLine();
+    mostrarnombre(nombre1);
+
+}
