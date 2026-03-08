bc9f6e897b2ac77b5bbd29f3005ebe1d6c7a03b9
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-08 17:49:42 -0600

    ejercicio 14 listo

---
+++ act11 14/Program.cs
@@ -0,0 +1,12 @@
+﻿static double dividir(int a, int b)
+{
+    return a / b;
+}
+int n1, n2;
+double resultado;
+Console.WriteLine("INGRESE EL HA DIVIDIR NUMERO:");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE POR CUANTO SE VA A DIVIDIR");
+n2 = int.Parse(Console.ReadLine());
+resultado = dividir(n1, n2);
+Console.WriteLine("EL RESULTADO DE LA SUMA ES: " + resultado);
