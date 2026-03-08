a0d0ffb565473fa1b0f2fa6addaff23273918934
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-08 17:10:58 -0600

    ejercicio 12 listo

---
+++ act11 12/Program.cs
@@ -0,0 +1,13 @@
+﻿static int restar(int a, int b)
+{
+    return a - b;
+}
+int n1, n2, resultado;
+Console.WriteLine("INGRESE EL PRIMER NUMERO:");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE EL SEGUNDO NUMERO");
+n2 = int.Parse(Console.ReadLine());
+
+resultado = restar(n1, n2);
+
+Console.WriteLine("EL RESULTADO DE LA RESTA ES: " + resultado);
