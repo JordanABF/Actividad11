92520ea631a39698414fbbc0ac630ed084c03331
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-08 16:28:54 -0600

    ejercicio 10 listo

---
+++ act11 10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("INGRESE UN TEXTO: ");
+string texto = Console.ReadLine();
+Console.WriteLine("INGRESE CUANTAS VECES DESEA REPETIR EL TEXTO: ");
+int repetir = int.Parse(Console.ReadLine());
+string imprimirrepetido(string texto, int veces)
+{
+    string resultado = "";
+    for (int i = 0; i < veces; i++)
+    {
+        Console.WriteLine((i+1) + " " + texto);
+    }
+    return resultado;
+
+}
+Console.WriteLine(imprimirrepetido(texto, repetir));
