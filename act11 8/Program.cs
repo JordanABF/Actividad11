4b60b062cdc4bc2013331c939ace6869740a1ad1
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-08 13:53:21 -0600

    actividad 8 listo

---
+++ act11 8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ingrese el cuadarado de un numero");
+double cuadradonumero(int n)
+{
+   return Math.Pow(n, 2);
+}
+int numero=int.Parse(Console.ReadLine());
+double r;
+
+r = cuadradonumero(numero);
+Console.WriteLine(r);
