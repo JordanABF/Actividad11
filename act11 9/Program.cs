5b2e5944c407612ee4a215c8ac130e450febdc02
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-08 16:11:45 -0600

    ejercicio 9 listo

---
+++ act11 9/Program.cs
@@ -0,0 +1,12 @@
+﻿static int sumar(int a,int b)
+{
+    return a + b;
+}
+int n1, n2,resultado;
+Console.WriteLine("INGRESE EL PRIMER NUMERO:");
+n1=int.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE EL SEGUNDO NUMERO");
+n2=int.Parse(Console.ReadLine());
+resultado=sumar(n1,n2);
+Console.WriteLine("EL RESULTADO DE LA SUMA ES: " + resultado);
+
