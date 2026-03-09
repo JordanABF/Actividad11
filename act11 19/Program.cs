6b02a85f22be283763bf7bba2c42b0c3071fff25
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-08 18:38:25 -0600

    ejercicio 19 listo

---
+++ act11 19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("INGRESE EL PRIMER NUMERO");
+int n1=int.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE EL SEGUNDO NUMERO");
+int n2=int.Parse(Console.ReadLine());
+static string multiplo(int n1, int n2)
+{
+    string resultado = "";
+    if (n1 % n2 == 0)
+    {
+        resultado = n1 + " es multiplo de " + n2;
+    }
+    else
+    {
+        resultado = n1 + " no es multiplo de " + n2;
+    }
+    return resultado;
+}
+
+Console.WriteLine(multiplo(n1, n2));
