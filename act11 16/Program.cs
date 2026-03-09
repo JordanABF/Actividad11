5266069a430bb59d1b78234dd5d5c68e4ed60f99
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-08 18:14:29 -0600

    ejercicio 16 listo

---
+++ act11 16/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Ingrese un numero");
+static string espositivo(int n)
+{
+    string resultado = "";
+    if (n > 0)
+    {
+       resultado="El numero es positivo";
+    }
+    else
+    {
+       resultado="El numero es negativo";
+    }
+    return resultado;
+}
+
+int n=int.Parse(Console.ReadLine());
+string resultado=espositivo(n);
+Console.WriteLine(resultado);
