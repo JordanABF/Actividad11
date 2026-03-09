53d350d2a6bd6bd31dc2538493d77ca02cbeb68f
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-08 18:23:25 -0600

    ejercicio 17 listo

---
+++ act11 17/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("INGRESE EL PRIMER NUMERO");
+int n1=int.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE EL SEGUNDO NUMERO");
+int n2=int.Parse(Console.ReadLine());
+static string mayor(int n1,int n2)
+{
+    string resultado="";
+    if (n1>n2)
+    {
+        resultado="El numero mayor es: "+n1;
+    }
+    else if (n2>n1)
+    {
+        resultado="El numero mayor es: "+n2;
+    }
+    else
+    {
+        resultado="Los numeros son iguales";
+    }
+    return resultado;
+}
+
+Console.WriteLine( mayor(n1,n2));
